dc7dc88ce96b15ea2efd0de16e1e09be34b8f34c
Donewell <donewellhart@gmail.cok>
2025-08-21 16:01:19 +0200

    pro settings allowed host update

--- ngoma/settings/pro.py
+++ ngoma/settings/pro.py
@@ -4,7 +4,7 @@ DEBUG = True
 ADMINS = (
         ('Donewell', 'donewell@brandsbydart.com'),
         )
-ALLOWED_HOSTS = ['ngoma.co.zw', 'www.ngoma.co.zw','127.0.0.1']
+ALLOWED_HOSTS = ['ngoma.co.zw', 'www.ngoma.co.zw','127.0.0.1', 'ngoma-lwo3.onrender.com']
 DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.postgresql',
